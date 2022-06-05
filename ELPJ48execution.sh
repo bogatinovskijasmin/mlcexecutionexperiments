041d37d237ea1cc92088145e4684a0ff2d2ff784
bogatinovskijasmin <jasmin.bogatinovski@gmail.com>
2022-06-05 17:13:06 +0200

    Update ELPJ48execution.sh

--- ELPJ48execution.sh
+++ ELPJ48execution.sh
@@ -1,8 +1,8 @@
 cd ./CHD_49/ELPJ48/ 
-tar xvzf scikit_ml_learn_data.zip
+tar xvzf scikit_ml_learn_data.tar.gz
 python3.5 execution_script.py CHD_49 ELPJ48 test
 python3.5 parseJsonFiles.py CHD_49 ELPJ48 
 rm -r ProcessedDatasets 
 rm -r scikit_ml_learn_data 
 rm scikit_ml_learn_data.tar.gz 
-tar -czf results_CHD_49_ELPJ48.zip *
+tar -czf results_CHD_49_ELPJ48.tar.gz *
